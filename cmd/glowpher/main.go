49a52654582911e1cf0efdacaf9f209a1da514ee
Tom Mitchell <tom@tom.net.nz>
2017-11-26 17:56:50 -0500

    Fix

--- cmd/glowpher/main.go
+++ cmd/glowpher/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
 	"github.com/tomnz/glowpher/internal/devices"
 )
@@ -11,18 +12,24 @@ func main() {
 	fmt.Println("hello world!")
 	device := devices.Registry["ws281x"]
 	device.Setup(10)
+	i := 0
 	for {
-		device.ShowColors([]uint32{
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-			rand.Uint32(),
-		})
+		if i < 1000 {
+			device.ShowColors([]uint32{
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+				rand.Uint32(),
+			})
+		} else {
+			time.Sleep(time.Second * 10)
+			i = 0
+		}
 	}
 }
