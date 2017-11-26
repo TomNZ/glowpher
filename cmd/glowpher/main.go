a3990332f756e59ed102d95893362b372ce8eb8e
Tom Mitchell <tom@tom.net.nz>
2017-11-26 13:35:55 -0500

    Fix

--- cmd/glowpher/main.go
+++ cmd/glowpher/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"time"
-	"ws2811"
+
+	"github.com/tomnz/glowpher/internal/rpiws281x"
 )
 
 func main() {
 	fmt.Println("hello world!")
-	ws2811.Init(18, 10, 255)
-	ws2811.SetBitmap([]uint32{
+	rpiws281x.Init(18, 10, 255)
+	rpiws281x.SetBitmap([]uint32{
 		1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
 	})
-	ws2811.Render()
+	rpiws281x.Render()
 	time.Sleep(time.Second * 5)
-	ws2811.Fini()
+	rpiws281x.Fini()
 }
