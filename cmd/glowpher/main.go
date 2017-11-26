2e2dcd0a245da62271d02861c8f9abf6c8ded38c
Tom Mitchell <tom@tom.net.nz>
2017-11-26 13:17:01 -0500

    Fix

---
+++ cmd/glowpher/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"ws2811"
+)
+
+func main() {
+	fmt.Println("hello world!")
+	ws2811.Init(18, 10, 255)
+	ws2811.SetBitmap([]uint32{
+		1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
+	})
+	ws2811.Render()
+	time.Sleep(time.Second * 5)
+	ws2811.Fini()
+}
