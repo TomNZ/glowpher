e9329db9a155245a39598147e86aa5fd0d172e34
Tom Mitchell <tom@tom.net.nz>
2017-11-26 13:23:38 -0500

    Fix

--- cmd/glowpher/main.go
+++ cmd/glowpher/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"ws2811"
 )
 
@@ -9,7 +10,7 @@ func main() {
 	fmt.Println("hello world!")
 	ws2811.Init(18, 10, 255)
 	ws2811.SetBitmap([]uint32{
-		1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
+		1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000,
 	})
 	ws2811.Render()
 	time.Sleep(time.Second * 5)
